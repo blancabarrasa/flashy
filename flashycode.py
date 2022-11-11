302260286b07778dfb95759f6159a0cd5414dc23
Blanca Barrasa <95690380+blancabarrasa@users.noreply.github.com>
2022-11-11 19:16:05 +0000

    Update flashycode.py

--- flashycode.py
+++ flashycode.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.markdown("<h1 style='text-align: center; color: white; font-family:verdana;'>Some title</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: white; font-family:montserrat;'>Flashy</h1>", unsafe_allow_html=True)
 
